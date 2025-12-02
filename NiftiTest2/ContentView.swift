b6342c3e9fb76684b40c801b5a50430129c0a4fa
Zaher Rezai <zaherrezaibfw@gmail.com>
2025-12-02 14:05:42 +0100

    Initial Commit

---
+++ NiftiTest2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NiftiTest2
+//
+//  Created by Zaher Rezai on 02/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NiftiTest2/NiftiTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  NiftiTest2App.swift
+//  NiftiTest2
+//
+//  Created by Zaher Rezai on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct NiftiTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
